a6bce9c72bfc18c391de853730fb988c232a67af
Kolin63 <theverylastgoodname@gmail.com>
2025-08-23 15:21:35 -0400

    Remove Gaps

--- config.def.h
+++ config.def.h
@@ -2,19 +2,11 @@
 
 /*
    Kolin63's dwm build
-
-   patches applied:
-   dwm-vanitygaps-20190508-6.2.diff
 */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
@@ -88,10 +80,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
- 	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +50 } },
- 	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -50 } },
- 	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
- 	{ MODKEY|Mod1Mask,              XK_0,      defaultgaps,    {0} },
 	{ MODKEY|ShiftMask,             XK_plus,   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
