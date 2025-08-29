ba65ac2ef478f71bb918793e1baf28654f7d0012
Kolin63 <theverylastgoodname@gmail.com>
2025-08-29 03:10:15 -0400

    Formatting

--- config.def.h
+++ config.def.h
@@ -109,12 +109,16 @@ static const Key keys[] = {
   { 0,                            XF86XK_AudioNext, spawn,   {.v = musicnext } },
 
   { MODKEY,                       XK_b,      togglebar,      {0} },
+
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
   { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+
   { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
   { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+
   { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
   { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+
   { MODKEY|ShiftMask,             XK_plus,   zoom,           {0} },
   { MODKEY,                       XK_Tab,    view,           {0} },
   { MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
