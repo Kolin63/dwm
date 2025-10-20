ebff17d42660a52df4d2d92da615e4b06ca9c6a4
Kolin63 <theverylastgoodname@gmail.com>
2025-10-20 00:59:04 -0400

    Update Keybinds

--- config.h
+++ config.h
@@ -95,9 +95,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]      = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]       = { "alacritty", NULL };
-static const char *browcmd[]       = { "qutebrowser", NULL };
-static const char *mousecmd[]      = { "mouse", NULL };
 
 static const char *volumeupcmd[]   = { "amixer", "-D", "pulse", "set", "Master", "5%+", NULL };
 static const char *volumedowncmd[] = { "amixer", "-D", "pulse", "set", "Master", "5%-", NULL };
@@ -115,9 +112,12 @@ static const char *screenshotinteractive[] = { "sh", "-c", "gnome-screenshot -c 
 static const Key keys[] = {
   /* modifier                     key        function        argument */
   { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-  { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-  { MODKEY,                       XK_semicolon, spawn,       {.v = browcmd } },
-  { MODKEY,                       XK_c,      spawn,          {.v = mousecmd } },
+  { MODKEY,                       XK_Return, spawn,          {.v = (const char*[]){ "alacritty", NULL } } },
+  { MODKEY,                       XK_semicolon, spawn,       {.v = (const char*[]){ "qutebrowser", NULL } } },
+
+  { MODKEY,                       XK_c,      spawn,          {.v = (const char*[]){ "mousetoggle", NULL } } },
+  { MODKEY|ShiftMask,             XK_c,      spawn,          {.v = (const char*[]){ "ansiicolor", NULL } } },
+  { MODKEY,                       XK_e,      spawn,          {.v = (const char*[]){ "emojiselect", NULL } } },
 
   { 0,                            XF86XK_AudioRaiseVolume, spawn, {.v = volumeupcmd } },
   { 0,                            XF86XK_AudioLowerVolume, spawn, {.v = volumedowncmd } },
@@ -180,7 +180,6 @@ static const Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
   { ClkWinTitle,          0,              Button2,        zoom,           {0} },
-  { ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
   { ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
   { ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
   { ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
