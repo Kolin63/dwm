0eda5011e5b7ac79d94c615b405ce725007ff1a2
Kolin63 <theverylastgoodname@gmail.com>
2025-10-26 12:25:34 -0400

    Revert "Remove Status Scripts"
    This reverts commit 596be855d0bd4d86092c9735a49ddaeaf902122c.

---
+++ daemonstatus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+while true; do
+  ~/.dotfiles/dwm/status.sh
+  sleep 0.5
+done
---
+++ status.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+if lsof +D ~/Videos 2>/dev/null | grep -q obs; then
+  obs=" Recording "
+fi
+
+spotify_metadata=$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:"org.mpris.MediaPlayer2.Player" string:"Metadata")
+spotify_artist=$(echo "$spotify_metadata" | grep -A 2 "xesam:artist" | tail -1 | cut -d '"' -f 2)
+spotify_title=$(echo "$spotify_metadata" | grep -A 1 "xesam:title" | tail -1 | cut -d '"' -f 2)
+if [[ ! "$spotify_title" == "" ]]; then
+  spotify=" $spotify_title - $spotify_artist "
+fi
+
+battery_raw=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0)
+battery_present=""
+battery_state=""
+battery_time_to_empty=""
+battery_time_to_full=""
+battery_percentage=""
+
+if [[ $battery_raw =~ present:[[:space:]]*([[:alnum:]]+) ]]; then
+  battery_present="${BASH_REMATCH[1]}"
+fi
+if [[ $battery_raw =~ state:[[:space:]]*([[:alpha:]-]+) ]]; then
+  battery_state="${BASH_REMATCH[1]}"
+fi
+if [[ $battery_raw =~ time\ to\ empty:[[:space:]]*([0-9]+\.[0-9]+|[0-9]+)[[:space:]]+([[:alpha:]]+) ]]; then
+  battery_time_to_empty="${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
+fi
+if [[ $battery_raw =~ time\ to\ full:[[:space:]]*([0-9]+\.[0-9]+|[0-9]+)[[:space:]]+([[:alpha:]]+) ]]; then
+  battery_time_to_full="${BASH_REMATCH[1]} ${BASH_REMATCH[2]}"
+fi
+if [[ $battery_raw =~ percentage:[[:space:]]*([0-9]+)% ]]; then
+  battery_percentage="${BASH_REMATCH[1]}"
+fi
+
+if [[ "$battery_present" == "yes" ]]; then
+  if [[ "$battery_state" == "charging" ]]; then
+    battery="󰂄 $battery_percentage%"
+  elif [ $battery_percentage -le 10 ]; then
+    battery="󰁺 $battery_percentage%"
+  elif [ $battery_percentage -le 20 ]; then
+    battery="󰁻 $battery_percentage%"
+  elif [ $battery_percentage -le 30 ]; then
+    battery="󰁼 $battery_percentage%"
+  elif [ $battery_percentage -le 40 ]; then
+    battery="󰁽 $battery_percentage%"
+  elif [ $battery_percentage -le 50 ]; then
+    battery="󰁾 $battery_percentage%"
+  elif [ $battery_percentage -le 60 ]; then
+    battery="󰁿 $battery_percentage%"
+  elif [ $battery_percentage -le 70 ]; then
+    battery="󰂀 $battery_percentage%"
+  elif [ $battery_percentage -le 80 ]; then
+    battery="󰂁 $battery_percentage%"
+  elif [ $battery_percentage -le 90 ]; then
+    battery="󰂂 $battery_percentage%"
+  elif [ $battery_percentage -le 100 ]; then
+    battery="󰁹 $battery_percentage%"
+  fi
+  battery+=" $battery_time_to_empty$battery_time_to_full "
+fi
+
+volume_raw=$(amixer -D pulse get Master)
+volume_perc=""
+if [[ $volume_raw =~ \[([0-9]+)%\] ]]; then 
+  volume_perc="${BASH_REMATCH[1]}"
+fi
+if   [ $volume_perc -eq 00 ]; then
+  volume="  $volume_perc% "
+elif [ $volume_perc -le 50 ]; then
+  volume="  $volume_perc% "
+else
+  volume="  $volume_perc% "
+fi
+
+date=$(date +"%a %b %d %Y ")
+time=$(date +"%I:%M %p")
+
+status+="\x06 $obs"
+status+="\x09 $spotify"
+status+="\x03 $battery"
+status+="\x0a $volume"
+status+="\x03 $date"
+status+="\x08 $time"
+xsetroot -name "$(echo -e $status)"
